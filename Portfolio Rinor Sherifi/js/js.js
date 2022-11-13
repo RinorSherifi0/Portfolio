4b5835c0a6ea85278651a9f4c7f6e23d7bcb977e
RinorSherifi0 <114572320+RinorSherifi0@users.noreply.github.com>
2022-11-13 18:27:41 +0100

    Add files via upload

---
+++ Portfolio Rinor Sherifi/js/js.js
@@ -0,0 +1,13 @@
+function dropdown() {
+    const x = document.querySelector(".hamburger-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+ function emaile(){
+ document.getElementById("contenti").innerHTML='<h3 class="section-title">Thanks for contacting me</h3>'
+ }
+ 
